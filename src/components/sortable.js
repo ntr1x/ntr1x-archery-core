03e4100075b99ea335cb66da44ce0bbb3fda3141
ipavlenko <ipavlenko@inbox.ru>
2016-07-16 21:21:42 +0500

    Structure update

---
+++ src/components/sortable.js
@@ -0,0 +1,332 @@
+(function ($, window, pluginName, undefined) {
+
+    var defaults = {
+
+        drag: true,
+        drop: true,
+        vertical: true,
+
+        containerSelector: "ol, ul",
+        itemSelector: "li",
+        excludeSelector: "li.disabled",
+
+        bodyClass: "dragging",
+        activeClass: "active",
+        draggedClass: "dragged",
+        verticalClass: "vertical",
+        horisontalClass: "horisontal",
+        placeholderClass: "placeholder",
+
+        placeholder: '<li class="placeholder"></li>',
+
+        onDragStart: function(context, event, _super) {
+
+            var size = {
+                height: context.$item.outerHeight(),
+                width: context.$item.outerWidth(),
+            };
+
+            context.$originalItem = context.$item;
+
+            context.$item = context.$originalItem
+                .clone()
+                .addClass(context.sortable.options.draggedClass)
+                .css({
+                    position: 'fixed',
+                    left: event.pageX - context.adjustment.left,
+                    top: event.pageY - context.adjustment.top,
+                    width: size.sidth,
+                    height: size.height,
+                })
+                .appendTo(context.$parent)
+            ;
+        },
+
+        onDrag: function(context, event, _super) {
+
+            context.$item.css({
+                left: event.pageX - context.adjustment.left,
+                top: event.pageY - context.adjustment.top,
+            })
+        },
+
+        onDrop: function(context, event, _super) {
+
+            context.$item.remove();
+            if (context.location) {
+
+                context.$item = context.location.before
+                    ? context.$item.insertBefore(context.location.$item)
+                    : context.$item.insertAfter(context.location.$item)
+                ;
+
+                context.$item.css({
+                    position: '',
+                    left: '',
+                    top: '',
+                    width: '',
+                    height: '',
+                })
+            }
+
+        },
+
+        // isValidTarget: function($item, container) {
+        //     return true;
+        // },
+        //
+        // onDrag: function ($item, position, _super, event) {
+        //     $item.css(position)
+        // },
+        //
+        // onDragStart: function ($item, container, _super, event) {
+        //     $item.css({
+        //         height: $item.outerHeight(),
+        //         width: $item.outerWidth()
+        //     });
+        //     $item.addClass(container.group.options.draggedClass);
+        //     $("body").addClass(container.group.options.bodyClass);
+        // },
+        //
+        // onDrop: function ($item, container, _super, event) {
+        //     $item.removeClass(container.group.options.draggedClass).removeAttr("style");
+        //     $("body").removeClass(container.group.options.bodyClass);
+        // },
+        //
+        // onMousedown: function ($item, _super, event) {
+        //   if (!event.target.nodeName.match(/^(input|select|textarea)$/i)) {
+        //     event.preventDefault();
+        //     return true;
+        //   }
+        // },
+        //
+        // toggleListeners: function (method) {
+        //
+        //     var that = this,
+        //         events = ['drag','drop','scroll']
+        //
+        //     $.each(events,function  (i,event) {
+        //         that.$document[method](eventNames[event], that[event + 'Proxy'])
+        //     })
+        // },
+    };
+
+    var context = null;
+    var sortables = [];
+
+    function Sortable($element, options) {
+
+        this.$element = $element;
+        this.options = $.extend({}, defaults, options);
+
+        $element.on('mousedown', this.options.itemSelector, (e) => { this.handleStart(e); });
+        // $element.on('mouseenter', this.options.containerSelector, (e) => { this.handleEnter(e); });
+        // $element.on('mouseleave', this.options.containerSelector, (e) => { this.handleLeave(e); });
+
+        this.draggable = null;
+
+        sortables.push(this);
+
+        console.log(JSON.parse(JSON.stringify(this.options)));
+    }
+
+    $(document).ready(function() {
+        $(document)
+            .on('mouseup', (e) => { context && context.sortable.handleEnd(e, context); })
+            .on('mousemove', (e) => { context && context.sortable.handleDrag(e, context); })
+        ;
+    });
+
+    Sortable.prototype = {
+
+        dropLocation: function(e) {
+
+            var $item;
+            var sortable;
+
+            if (context) {
+
+                var display = context.$item.css('display');
+                context.$item.css({ display: 'none', });
+
+                for (var i = 0; i < sortables.length; i++) {
+                    var s = sortables[i];
+                    if (s.options.drop) {
+                        $result = $(document.elementFromPoint(e.pageX, e.pageY)).closest(s.options.itemSelector);
+                        if ($result.length && $result.closest(s.$element).length) {
+                            $item = $result;
+                            sortable = s;
+                            break;
+                        }
+                    }
+                }
+
+                context.$item.css({ display: display, });
+
+            } else {
+
+                for (var i = 0; i < sortables.length; i++) {
+                    var s = sortables[i];
+                    if (s.options.drop) {
+                        $result = $(document.elementFromPoint(e.pageX, e.pageY)).closest(s.options.itemSelector);
+                        if ($result.length && $result.closest(s.$element).length) {
+                            $item = $result;
+                            sortable = s;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (sortable && $item && $item.length) {
+
+                var $container = $item.closest(sortable.options.containerSelector);
+
+                var offset = $item.offset();
+                var size = {
+                    width: $item.outerWidth(),
+                    height: $item.outerHeight(),
+                };
+
+                var orientation = this.options.vertical
+                    ? $container.hasClass(sortable.options.horisontalClass) ? 'h' : 'v'
+                    : $container.hasClass(sortable.options.verticalClass) ? 'v' : 'h'
+                ;
+
+                var before = (orientation == 'h')
+                    ? e.pageX - offset.left < size.width / 2
+                    : e.pageY - offset.top < size.height / 2
+                ;
+
+                return {
+                    $item: $item,
+                    $container: $container,
+                    sortable: sortable,
+                    before: before,
+                };
+            }
+
+            return null;
+        },
+
+        handleStart: function(e) {
+
+            e.preventDefault();
+            e.stopPropagation();
+
+            if (!context) {
+
+                var $item = $(e.target).closest(this.options.itemSelector);
+                var $parent = $item.parent();
+
+                var offset = $item.offset();
+
+                context = {
+                    sortable: this,
+                    index: $item.index(),
+                    $container: $item.closest(this.options.containerSelector),
+                    $parent: $item.parent(),
+                    $item: $item,
+                    $originalItem: $item,
+                    $targetItem: null,
+                    $targetContainer: null,
+                    location: this.dropLocation(e),
+                    adjustment: {
+                        left: e.clientX - offset.left,
+                        top: e.clientY - offset.top,
+                    },
+                };
+
+                this.options.onDragStart(context, e, defaults.onDragStart);
+            }
+        },
+
+        handleEnd: function(e) {
+
+            if (context) {
+
+                for (var i = 0; i < sortables.length; i++) {
+                    var sortable = sortables[i];
+                    $(sortable.options.containerSelector, sortable.$element).removeClass(sortable.options.activeClass);
+                }
+
+                if (context.$placeholder) {
+                    context.$placeholder.remove();
+                }
+
+                context.location = this.dropLocation(e);
+                if (context.location) {
+                    context.location.sortable.options.onDrop(context, e, defaults.onDrop);
+                } else {
+                    context.$item.remove();
+                }
+
+                context = null;
+            }
+        },
+
+        handleDrag: function(e) {
+
+            if (context) {
+
+                for (var i = 0; i < sortables.length; i++) {
+                    var sortable = sortables[i];
+                    $(this.options.containerSelector, sortable.$element).removeClass(this.options.activeClass);
+                }
+
+                if (context.$placeholder) {
+                    context.$placeholder.remove();
+                }
+
+                context.location = this.dropLocation(e);
+                if (context.location) {
+                    context.location.$container.addClass(context.location.sortable.options.activeClass);
+                    context.$placeholder = context.location.before
+                        ? $(context.location.sortable.options.placeholder).insertBefore(context.location.$item)
+                        : $(context.location.sortable.options.placeholder).insertAfter(context.location.$item)
+                    ;
+                }
+
+                context.sortable.options.onDrag(context, e, defaults.onDrag);
+            }
+        },
+    };
+
+    var API = $.extend(Sortable.prototype, {
+
+        enable: function() {
+        },
+        disable: function () {
+        },
+        destroy: function () {
+        }
+    });
+
+    /**
+     * jQuery API
+     *
+     * Parameters are
+     *   either options on init
+     *   or a method name followed by arguments to pass to the method
+     */
+    $.fn[pluginName] = function(methodOrOptions) {
+
+        var args = Array.prototype.slice.call(arguments, 1);
+
+        return this.map(function() {
+
+            var $t = $(this),
+                object = $t.data(pluginName)
+            ;
+
+            if (object && API[methodOrOptions]) {
+                return API[methodOrOptions].apply(object, args) || this;
+            } else if (!object && (methodOrOptions === undefined || typeof methodOrOptions === "object")) {
+                $t.data(pluginName, new Sortable($t, methodOrOptions));
+            }
+
+            return this;
+        });
+    };
+
+})(jQuery, window, 'sortable');
